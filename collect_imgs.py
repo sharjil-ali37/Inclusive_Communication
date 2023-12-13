10173e1655a9b5825d448f80aa96df60ca064d65
sharjil-ali37 <106882955+sharjil-ali37@users.noreply.github.com>
2023-12-13 14:13:13 +0530

    Update collect_imgs.py

--- collect_imgs.py
+++ collect_imgs.py
@@ -1,20 +1,20 @@
 import os
 import cv2
-#Setting Up Directories:
+
 DATA_DIR = './data'
 if not os.path.exists(DATA_DIR):
     os.makedirs(DATA_DIR)
-#Setting Parameters:
+
 number_of_classes = 5
 dataset_size = 65
-#Creating Class Directories:Creates a directory for the current class inside the 'data' directory.
+
 cap = cv2.VideoCapture(0)
 for j in range(number_of_classes):
     if not os.path.exists(os.path.join(DATA_DIR, str(j))):
         os.makedirs(os.path.join(DATA_DIR, str(j)))
 
     print('Collecting data for class {}'.format(j))
-#User Prompt and Waiting for Key Press:
+
     done = False
     while not done:
         ret, frame = cap.read()
@@ -26,7 +26,7 @@ for j in range(number_of_classes):
         if key == ord('c'):
             done = True
     
-   #Capturing Images:
+
     counter = 0
     while counter < dataset_size:
         ret, frame = cap.read()
@@ -35,6 +35,6 @@ for j in range(number_of_classes):
         cv2.imwrite(os.path.join(DATA_DIR, str(j), '{}.jpg'.format(counter)), frame)
 
         counter += 1
-#Releasing Resources:
+
 cap.release()
 cv2.destroyAllWindows()
